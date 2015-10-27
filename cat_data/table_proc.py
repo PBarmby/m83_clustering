9bb3e2982430f53d4db135fb7fd26f5e07d764c7
Pauline Barmby <pbarmby@uwo.ca>
2015-10-27 15:38:24 -0400

    refinements to processing, to make NED/SIMBAD match easier

--- cat_data/table_proc.py
+++ cat_data/table_proc.py
@@ -3,8 +3,8 @@ import numpy as np
 import os
 
 #usage:
-#process_tab(ned_in, ned_out, type_col='Type',select_list = within_galaxy_types_ned)
-#process_tab(simbad_in, simbad_out, type_col='OTYPE_S',select_list = within_galaxy_types_simbad)
+#process_tab(ned_in, ned_out, type_col='Type',select_list = table_proc.within_galaxy_types_ned, rfmt_fn=table_proc.rfmt_ned)
+#process_tab(simbad_in, simbad_out, type_col='OTYPE_S',select_list = table_proc.within_galaxy_types_simbad,rfmt_fn=table_proc.rfmt_simbad)
 
 within_galaxy_types_ned = ['*Cl','HII','PofG','Neb','SN','SNR', 'V*','WR*']
 background_types_ned = ['G','GClstr','GGroup','QSO']
@@ -16,19 +16,40 @@ obs_types_simbad = ['EmObj','IR','Radio','Radio(sub-mm)','UV','X']
 
 
 
-def process_tab(tab_in, tab_out, type_col, select_list = within_galaxy_types_ned):
+def process_tab(tab_in, tab_out, type_col, select_list = within_galaxy_types_ned, rfmt_fn=None):
     '''select specific object types from a table'''
     tab = Table.read(tab_in)
-    tab['Type'] = np.char.strip(tab[type_col]) # remove whitespace -- helps with matching
-    if type_col != 'Type': # change over to use Type for any table
-        tab.delete_column(type_col)
+    if type_col != 'Type':
+        tab.rename_column(type_col, 'Type')
+    tab['Type'] = np.char.strip(tab['Type']) # remove whitespace -- helps with matching
     tg = tab.group_by('Type')
-    mask = np.in1d(tg.groups.keys[type_col],select_list) # create mask for only wanted types
+    mask = np.in1d(tg.groups.keys['Type'],select_list) # create mask for only wanted types
     wanted_types = tg.groups[mask]
 
-    if tab_out != None:
+    # do some reformatting
+    if rfmt_fn != None:
+        rfmt_fn(wanted_types)
+
+    if tab_out != None: # write to file
         if os.path.exists(tab_out):
             os.unlink(tab_out)
         wanted_types.write(tab_out, format='fits')
     return(wanted_types)
-    
+
+def rfmt_ned(in_tab):
+    ''' strip prefixes from some NED names to better match SIMBAD
+         obvs. very specific to this dataset'''
+    in_tab['Object Name'] = np.char.replace(in_tab['Object Name'],"MESSIER 083:","")
+    in_tab['Object Name'] = np.char.replace(in_tab['Object Name'],"NGC 5236:","")
+    in_tab['Object Name'] = np.char.replace(in_tab['Object Name'], " ", "") # strip spaces
+    in_tab.rename_column('Object Name','Name_NED')
+    return
+
+def rfmt_simbad(in_tab):
+    ''' change some object types to better match NED
+         obvs. very specific to this dataset'''
+    in_tab['Type'] = np.char.strip(in_tab['Type'],"?") # only applies to "SNR?"
+    in_tab['Type'][in_tab['Type']=='Cl*'] = '*Cl' 
+    in_tab.rename_column('MAIN_ID','Name_SIMBAD')
+    in_tab['Name_SIMBAD'] = np.char.replace(in_tab['Name_SIMBAD']," ", "") # strip spaces
+    return
