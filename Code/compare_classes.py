51f9821aa88166b16691cda6347ca1422cac886b
Pauline Barmby <pbarmby@uwo.ca>
2016-01-19 16:41:50 -0500

    stacked bar graph comparing 2 sets of classifications

--- Code/compare_classes.py
+++ Code/compare_classes.py
@@ -49,24 +49,39 @@ def plot_classes_v1(match_tab, class1, class2, class1_lab = 'NED', class2_lab='K
     fig.show()
     return
 
-# TODO: try a set of bar charts, where the horizontal axis is class1
-#  and the vertical axis counts the different classes in class2 (or vice versa)
+# intention: a set of bar charts, where the horizontal axis is class1
+#  and the vertical axis counts the different classes in class2 
 def plot_classes_v2(match_tab, class1, class2, class1_lab = 'NED', class2_lab='K-means'):
-    # group data by class1
-    grouped_tab = match_tab.group_by(class1)
-    class1_types = grouped_tab.groups.keys[class1]
-    ngroups_class1 = len(class1_types)
-    ngroups_class2 = len(unique(match_tab,class2))
+    # figure out how many different classes
+    ngroups_class1 = len(unique(match_tab, class1))
+    keys2 = unique(match_tab,class2)[class2]
+    ngroups_class2 = len(keys2)
+    n2in1 = np.zeros((ngroups_class1, ngroups_class2),dtype='int')              
 
+    # group by class2, figure out how many of each class2 in each class1 group
+    # surely there's a better way to do this, but I couldn't work it out
+    grouped_tab = match_tab.group_by(class1)
+    for i, grp in enumerate(grouped_tab.groups): 
+        for j in range(0,ngroups_class2):
+            n2in1[i,j] = len(grp[grp[class2]==keys2[j]]) 
+            
     # make plot
     fig,ax = plt.subplots()
-    # loop over types
-    for i, group in enumerate(grouped_tab.groups):
-        ax.plot(0*group[class2]+i, group[class2], label=class1_types[i], ls='None',marker='s')
+    c = ['b','g','r','c','y','m','0.5','k','blueviolet','chartreuse','crimson','darkslateblue','goldenrod','pink','brown','orange'] # set colour cycle
+    xind = np.arange(ngroups_class1) # set number of classes
+
+    # loop over class2
+    for j in range(0, ngroups_class2):
+        if j>0:
+            bot += n2in1[:,j-1] # sum up the bottom of the bars
+            ax.bar(xind, n2in1[:,j], bottom = bot, label=keys2[j],color=c[j])
+        else:
+            ax.bar(xind, n2in1[:,j], label=keys2[j],color=c[j])            
+            bot = n2in1[:,j]*0
     ax.set_xlabel(class1_lab, fontweight='bold',fontsize=14)
-    ax.set_ylabel(class2_lab, fontweight='bold',fontsize=14)
-    ax.set_xlim(-1,ngroups_class1+1)
-    ax.set_ylim(-1,ngroups_class2+1)
+    ax.set_ylabel('Number', fontweight='bold',fontsize=14)
+    plt.xticks(xind + 0.4,grouped_tab.groups.keys[class1])
+    ax.set_ylim(0,n2in1.sum(axis=1).max()*1.1)
     ax.legend()
     fig.show()
     return
