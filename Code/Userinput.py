d8561d479ab0c7b4f5a2c4e670e685fb7973216d
Alex Kiar <akiar@uwo.ca>
2016-02-18 09:55:32 -0500

    Command line inputs

--- Code/Userinput.py
+++ Code/Userinput.py
@@ -4,50 +4,30 @@ Created on Sun Dec 13 12:19:08 2015
 
 @author: Owner
 """
-
-def main(): 
-    
-    print "**Please enter a space between each input**" 
-    analysis = raw_input("What analysis would you like to perform? (meanshift, kmeans, mst): ")
-    make_plots = raw_input("What plots would you like(cluster, color, xy, mst): ")
-    classification_ID = raw_input("Would you like the objects to be catalogued (Yes/No): ")
-    make_results_summary = raw_input("Would you like a results summary (Yes/No): ")
-    
-    if analysis in ['kmeans']:
-        meanshift_as_input_to_KMEANS = raw_input("Would you like to use the number of clusters estimated by MEANSHIFT as input to KMEANS? (Yes/No): ")
-    
-    #Save the inputs 
-    
-    inputs = [analysis, make_plots, classification_ID, make_results_summary]
-    print inputs[0]
-    #Check Inputs
-
-    for i in range (0,3):
-    
-        while inputs[i] not in ['meanshift', 'kmeans', 'mst', 'cluster', 'color', 'xy', 'mst', 'Yes', 'yes', 'No', 'no']: 
-       
-            print "Invalid input, please try again."
-            print "**Please enter a space between each input**"
-            analysis = raw_input("What analysis would you like to perform? (meanshift, kmeans, mst): ")
-            make_plots = raw_input("What plots would you like(cluster, color, xy, mst): ")
-            classification_ID = raw_input("Would you like the objects to be catalogued (Yes/No): ")
-            make_results_summary = raw_input("Would you like a results summary (Yes/No): ")
-    
-    confirm = raw_input("Start clustering now? (Yes/Change my inputs): ")
-    
-    while confirm not in ['Yes', 'yes']:
-    
-        print "**Please enter a space between each input**" 
-        analysis = raw_input("What analysis would you like to perform? (meanshift, kmeans, mst): ")
-        make_plots = raw_input("What plots would you like(cluster, color, xy, mst): ")
-        classification_ID = raw_input("Would you like the objects to be catalogued (Yes/No): ")
-        make_results_summary = raw_input("Would you like a results summary (Yes/No): ")
-        
-        confirm = raw_input("Start clustering now? (Yes/Change my inputs): ")
-    
+import argparse
 
     
-    return
+inputs = argparse.ArgumentParser()
+inputs.add_argument("data_file", help="Choose the data file for analysis")
+inputs.add_argument("analysis", help = "Choose the methods you would like to use", choices=['meanshift', 'kmeans', 'mst'])
+inputs.add_argument("-msi", "--kmeans_input", help="Choose the number of clusters input for kmeans", choices=['meanshift', 'experiments.txt'])
+inputs.add_argument("-p","--plots", help = "Choose the plots you would like to make", choices =['meanshift', 'kmeans_color', 'kmeans_xy', 'mst'], default ="none")
+inputs.add_argument("-id", "--id_list", help = "Produces object id list", choices = ['yes','no'], default="no")
+inputs.add_argument("-rs", "--results_summary", help="Produces results summary", choices =['yes','no'], default="no")
+inputs.add_argument("-s", "--save_results", help="Enter path for saving output files", default="no")    
+    
+criteria = inputs.parse_args()    
+
+if 'meanshift' in criteria.analysis: 
+    print 'meanshift' 
+elif 'kmeans' in criteria.analysis:
+    print 'kmeans' 
+elif 'mst' in criteria.analysis:
+    print 'mst' 
+else: 
+    print criteria.data_file
+    
+
 
 
 
