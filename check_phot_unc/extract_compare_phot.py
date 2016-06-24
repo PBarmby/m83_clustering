3765a5b43953b6f2082df3d0f5b89c17d8249238
Pauline Barmby <pbarmby@uwo.ca>
2016-06-24 14:15:12 -0400

    changes to plots, results seem ok

--- check_phot_unc/extract_compare_phot.py
+++ check_phot_unc/extract_compare_phot.py
@@ -36,48 +36,60 @@ def go(filt_list = ubvi_list, zps=ubvi_zps, ers = '../raw_data/hlsp_wfc3ers_hst_
         new_cat = Table.read(catname, format = 'ascii')
         new_coo = SkyCoord(ra=(new_cat['col3']+ubvi_pos_offsets[i][0])*u.degree, dec=(new_cat['col4']+ubvi_pos_offsets[i][1])*u.degree) 
 
-        # match results with ERS catalog
+        # match SE results with ERS catalog
+        # NB: matching results cross-checked vs TOPCAT: same
         idx, sep2d, sep3d = match_coordinates_sky(new_coo, ers_coo) 
         matched = sep2d < match_tol*u.arcsec
         print('{}: matched {} of {} objects'.format(catname, matched.sum(), gooddat.sum()))
 
-#        # compute magnitude/uncert offsets
+#        # pull out data for comparison
         m1_old = ers_subcat[m1][idx][matched]
         m2_old = ers_subcat[m2][idx][matched]
-        dm1 = (ers_subcat[m1][idx] - new_cat['col5'])[matched]
-        dm2 = (ers_subcat[m2][idx] - new_cat['col6'])[matched]
-        u1 = ers_subcat[m1+'_unc'][idx][matched]
-        u2 = ers_subcat[m2+'_unc'][idx][matched]
+        m1_new = new_cat['col5'][matched]
+        m2_new = new_cat['col6'][matched]
+        u1_old = ers_subcat[m1+'_unc'][idx][matched]
+        u2_old = ers_subcat[m2+'_unc'][idx][matched]
+        u1_new = new_cat['col7'][matched]
+        u2_new = new_cat['col8'][matched]
 
-        # compute some statistics
+        # compute some binned statistics
         bin_edges = np.arange(18,30,0.5)
         bin_width = (bin_edges[1] - bin_edges[0])
-        bin_centers = bin_edges[1:] - bin_width/2
-        med_dm1, junk1, junk2 = binned_statistic(m1_old, dm1, 'median', bin_edges)
-        med_dm2, junk1, junk2 = binned_statistic(m2_old, dm2, 'median', bin_edges)
+        bin_c1 = bin_edges[1:] - bin_width/2
+        med_m1, junk1, junk2 = binned_statistic(m1_old, m1_new, 'median', bin_edges)
+        med_m2, junk1, junk2 = binned_statistic(m2_old, m2_new, 'median', bin_edges)
+        bin_edges = np.arange(0,3,0.25)
+        bin_width = (bin_edges[1] - bin_edges[0])
+        bin_c2 = bin_edges[1:] - bin_width/2
+        med_u1, junk1, junk2 = binned_statistic(u1_old, u1_new, 'median', bin_edges)
+        med_u2, junk1, junk2 = binned_statistic(u2_old, u2_new, 'median', bin_edges)
                 
         # make some plots
         fig, ax = plt.subplots(2,1)
-        ax[0].plot(m1_old, new_cat['col5'][matched], marker='.', ms=5, label= '0.5pix')
-        ax[0].plot(m2_old, new_cat['col6'][matched], marker='.', ms=5,label= '3pix')        
-        ax[0].plot(bin_centers, med_dm1, marker='None', ls='solid')
-        ax[0].plot(bin_centers, med_dm2, marker='None', ls='solid')
+        ax[0].plot(m2_old, m2_new, marker='.', ms=1,label= '3pix')        
+        ax[0].plot(m1_old, m1_new, marker='.', ms=1, label= '0.5pix')
+        ax[0].plot(bin_c1, med_m1, marker='None', ls='solid',color='lightgreen')
+        ax[0].plot(bin_c1, med_m2, marker='None', ls='solid',color='cyan')
         ax[0].set_xlim(18,30)
         ax[0].set_ylim(18,30)
-        ax[0].grid()
-        ax[0].set_xlabel('Catalog magnitude')
+        ax[0].grid(ls='dotted')
+        ax[0].set_xlabel('ERS catalog magnitude')
         ax[0].set_ylabel('New mag')
-        ax[0].legend(loc='upper left',numpoints=1)
-        ax[0].set_title(filt)
-        ax[1].plot(u1, new_cat['col7'][matched], marker='.', ms=5,label= '0.5pix')
-        ax[1].plot(u2, new_cat['col8'][matched], marker='.', ms=5,label= '3pix')        
-        ax[1].set_xlim(-0.1,10)
-        ax[1].set_ylim(-0.05,0.5)
-        ax[1].set_xlabel('Catalog magnitude unc')
+        ax[0].legend(loc='upper left',numpoints=1,markerscale=5)
+        ax[0].set_title('F{}W comparison'.format(filt))
+        ax[1].plot(u2_old, u2_new, marker='.', ms=1,label= '3pix')
+        ax[1].plot(u1_old, u1_new, marker='.', ms=1,label= '0.5pix')
+        ax[1].plot(bin_c2, med_u1, marker='None', ls='solid',color='lightgreen')
+        ax[1].plot(bin_c2, med_u2, marker='None', ls='solid',color='cyan')
+        ax[1].set_xlim(-0.1,4)
+        ax[1].set_ylim(-0.02,0.4)
+        ax[1].grid(ls='dotted')
+        ax[1].set_xlabel('ERS catalog magnitude unc')
         ax[1].set_ylabel('New mag unc')
         fig.tight_layout()
         figname = 'comp_{}.png'.format(filt)
         plt.savefig(figname)
+        plt.close(fig)
         
     #end of loop over images
     return
