85ca541efb755b548d91468354e6bef2d8faa096
Alex Kiar <akiar@uwo.ca>
2016-08-17 15:52:38 -0400

    Compare clustering plots

--- Code/compare_clustering.py
+++ Code/compare_clustering.py
@@ -4,7 +4,11 @@ import numpy as np
 import pylab
 from matplotlib import pyplot as plt
 from astropy.table import Table, join
-
+# df_1: name of id_file 
+# df_2: name of id_file
+# d(n)_path: path to data files
+# save_path: path to save plots
+# plots: 'corr', 'broad_pos'
 
 def compare_class(df_1, df_2, d1_path, d2_path, save_path, plots):
     survey = Table.read('data_v3.txt', format='ascii.commented_header',
@@ -12,22 +16,22 @@ def compare_class(df_1, df_2, d1_path, d2_path, save_path, plots):
     # Load two clusterings
     path1 = load_path(d1_path)
     path2 = load_path(d2_path)
-    save_dir = mk_dir(save_path)  # make the saving directory
+    s_path = load_path(save_path)
+    mk_dir(s_path)  # make the saving directory
     label_data, clust_1data, clust_2data = data(df_1, df_2, survey, path1,
-                                                path2)
-
+                                                path2, plots)
     if "corr" in plots:
-        corr_plot(label_data, save_dir)
+        corr_plot(label_data, s_path)
     if "broad_pos" in plots:
-        broad_plot(clust_1data, clust_2data, save_dir)
+        broad_plot(clust_1data, clust_2data, s_path)
 
     return
 
 
-def data(clustering1, clustering2, survey_data, path_1, path_2, plots):
-    clust1 = Table.read(path_1 + clustering1, format='ascii.commented_header',
+def data(clustering1, clustering2, survey_data, path_1, path_2, plts):
+    clust1 = Table.read(path_1 + '\\' + clustering1, format='ascii.commented_header',
                         guess=False)
-    clust2 = Table.read(path_2 + clustering2, format='ascii.commented_header',
+    clust2 = Table.read(path_2 + '\\' + clustering2, format='ascii.commented_header',
                         guess=False)
     clust1_data = join(clust1, survey_data, 'object_id')
     clust2_data = join(clust2, survey_data, 'object_id')
@@ -43,7 +47,7 @@ def mk_dir(path_):
 
 
 def load_path(path_):
-    path = 'C:\\Users\\Owner\\Documents\\GitHub\\m83_clustering\\{}'.format(path_)
+    path = 'C:\\Users\\Owner\\Documents\\GitHub\\m83_clustering\\results\\{}'.format(path_)
     return(path)
 
 
@@ -61,7 +65,8 @@ def corr_plot(labels, path_):
             ax.xaxis.set_major_locator(plt.MultipleLocator(1.0))
             ax.yaxis.set_major_locator(plt.MultipleLocator(1.0))
     plt.show()
-    pylab.savefig(path_ + 'similar_clusters.png')
+    file_name = 'similar_clusters.png'
+    pylab.savefig(os.path.join(path_, file_name))
     plt.close()
     return
 
