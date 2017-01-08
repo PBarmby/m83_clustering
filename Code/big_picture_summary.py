2081589635fb6f9eb17fbaf0766866b2958b0181
Pauline Barmby <pbarmby@uwo.ca>
2017-01-08 14:11:07 -0500

    example code from pylatex docs

--- Code/big_picture_summary.py
+++ Code/big_picture_summary.py
@@ -1,24 +1,46 @@
 from astropy.Table import table
 from glob import glob
-import os.file
+from pylatex import Document, Section, Subsection, Tabular, Math, TikZ, Axis, Plot, Figure, Matrix
+from pylatex.utils import italic
+import os
 
-# there is a pylatex package that might do a lot of the heavy lifting here..
+# combine figures and tables for a given clustering run into a single document
 
 texstart = "\begin{document}"
 
 def doit_2d(outfile = 'summary'):
     # generate blank LaTeX doc
-    texdoc = outfile+'.tex'
-    if os.file.is_file(texdoc):
-        os.unlink(texdoc)
+    geometry_options = {"tmargin": "1cm", "lmargin": "2cm"}
+    doc = Document(geometry_options=geometry_options)
 
-    outfile = file.open(texdoc, "w")
-    outfile.write(texstart)
-    
+# examples from documentation
+#    with doc.create(Section('The simple stuff')):
+#        doc.append('Some regular text and some')
+#        doc.append(italic('italic text. '))
+#        doc.append('\nAlso some crazy characters: $&#{}')
+#        with doc.create(Subsection('Math that is incorrect')):
+#            doc.append(Math(data=['2*3', '=', 9]))
+#
+#    with doc.create(Subsection('Table of something')):
+#            with doc.create(Tabular('rc|cl')) as table:
+#                table.add_hline()
+#                table.add_row((1, 2, 3, 4))
+#                table.add_hline(1, 2)
+#                table.add_empty_row()
+#                table.add_row((4, 5, 6, 7))
+
     # find the color-color plots
     kmeans_plots = glob('kmeans_color_*')
     
     # insert into a LaTex document
+
+# examples from documentation
+    with doc.create(Subsection('Cute kitten pictures')):
+        with doc.create(Figure(position='h!')) as kitten_pic:
+            kitten_pic.add_image(image_filename, width='120px')
+            kitten_pic.add_caption('Look it\'s on its back')
+
+
 
     # read the clustering results table and insert that too
     
@@ -26,12 +48,13 @@ def doit_2d(outfile = 'summary'):
 
     # insert into a LaTex document
 
-    # finish up the document
-    outfile.write(texend)
-    outfile.close()
+    # generate document name, delete old versions
+    texdoc = outfile+'.tex'
+    if os.file.is_file(texdoc):
+        os.unlink(texdoc)
 
     # turn the LaTex into a PDF
-    os.sysstr('pdflatex %s' % outfile)
+    doc.generate_pdf(outfile, clean_tex=False)
     
     # all done!
     return
@@ -42,10 +65,10 @@ def twofigs(file1, file2):
     fig_string += "\caption{Left: {}, Right: {}}" % use filename as caption?
     return(fig_string)
 
-def onefigs(file1):
-    fig_string = "\begin{figure}\n \includegraphics[0.45\linewidth]{}\includegraphics[0.45\linewidth]{}"
+def onefig(file1):
+    fig_string = "\begin{figure}\n \includegraphics{}"
     fig_string += "\end{figure}"
-    fig_string += "\caption{Left: {}, Right: {}}"
+    fig_string += "\caption{}"
     return(fig_string)
 
 
